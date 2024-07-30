02ed9905573850686ecd21528887989491bde3de
ham <s24018@gsm.hs.kr>
2024-07-30 23:29:58 +0900

    js study 1

---
+++ quiz/quiz.js
@@ -0,0 +1,19 @@
+const submit = document.querySelector(".submit-button");
+
+const quiz = [];
+
+quiz[0] = "함성우의 생일은? ex)1월 1일";
+quiz[1] = "함성우의 키는? (소수점 빼고)";
+quiz[2] = "함성우의 최애 음식은?";
+
+function firstQuiz() {
+    
+}
+
+function secondQuiz() {
+}
+
+function thirdQuiz() {
+}
+
+submit.addEventListener("click", submitClcik);
